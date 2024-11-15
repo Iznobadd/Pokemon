64d8ab318e64b0f671beefcf2c984e80ff199b9e
Iznobad <brandon.cebi.pro@gmail.com>
2024-11-15 14:11:00 +0100

    add bearer token to request

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -4,9 +4,26 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL as string,
 });
 
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
     if (error.response) {
       const errorMessage = error.response.data.message || "An error occurred";
       return Promise.reject({ ...error, message: errorMessage });
