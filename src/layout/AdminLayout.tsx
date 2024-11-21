e369d7749e5c0cf38010c2dac7cd1bd81cf8ba43
Iznobad <brandon.cebi.pro@gmail.com>
2024-11-21 21:37:23 +0100

    update admin layout

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -1,12 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Sidebar from "../components/admin/Sidebar";
 
 const AdminLayout = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-[#f1f5f9]">
       <Sidebar />
       <div className="relative flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
-        <main>
-          <div className="mx-auto max-w-screen-2xl p-10"></div>
+        <main className="">
+          <div className="mx-auto max-w-screen-2xl p-10">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>
